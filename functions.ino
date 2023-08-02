cc77e712e8a907d22208d96084a6772d43f4b0de
erics80 <54673850+erics80@users.noreply.github.com>
2023-08-02 13:11:17 -0400

    Update functions.ino

--- functions.ino
+++ functions.ino
@@ -1,1 +1,8 @@
 //write any useful functions that we can use in here
+
+//Example
+//Takes in a temperature in celsius and converts it to fahrenheit
+double CtoF(double temp){
+  Ftemp = temp*9/5 + 32;
+  return Ftemp;
+}
