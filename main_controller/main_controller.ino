ea332cfadc620caea39107b2046188dca6508708
erics80 <54673850+erics80@users.noreply.github.com>
2023-08-11 13:12:10 -0400

    Update main_controller.ino
    Changing ADC and PWM Resolution to 12 bits

--- main_controller/main_controller.ino
+++ main_controller/main_controller.ino
@@ -2,6 +2,8 @@
 //main program for arduino controller
 
 void setup() {
+  analogReadResolution(12); //changing the ADC resolution to 12 bits. ES
+  analogWriteResolution(12); //changing the PWM output resolution to 12 bits. ES
 }
 
 void loop(){
