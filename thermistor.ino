b40b90836fba2a430dbf187b2a1e976abdd747a0
erics80 <54673850+erics80@users.noreply.github.com>
2023-08-17 15:04:41 -0400

    Update thermistor.ino

--- thermistor.ino
+++ thermistor.ino
@@ -4,13 +4,16 @@ double temp;
   double r0 = 10000;
   double r1 = 10000;
   for(int i = 0; i < 5; i++){
-  if((double)analogRead(pin) != 0){
-  rt = r1 * (4095.00/(double)analogRead(pin))- 1.00;
+  if(analogRead(pin) != 0 && analogRead(pin) != 4095){
+  rt = r1 * (4095.00/analogRead(pin)-1.00);
   temp += (1/((1/298.15)+(1/beta) * log(rt/r0)))-273.15;
     
-  } else { //divide by 0 case
+  } else if(pin == 4095){
+  rt = r1 * (4095.00/4094-1.00);
+  temp += (1/((1/298.15)+(1/beta) * log(rt/r0)))-273.15;
+  }else { //divide by 0 case
     
-  rt = r1 * (4095.00/1.00)- 1.00;
+  rt = r1 * (4095.00/1-1.00);
   temp += (1/((1/298.15)+(1/beta) * log(rt/r0)))-273.15;
   }
   }
