bdf890793d858f72e5eaee7d702936d83487c849
erics80 <54673850+erics80@users.noreply.github.com>
2023-08-22 15:32:38 -0400

    Update thermistor.ino
    Merged equation steps to use less variables and save memory

--- thermistor.ino
+++ thermistor.ino
@@ -23,56 +23,34 @@ double temp;
 double thermistorRead(int pin, int B25_50, int B25_100){
 double temp;
   double rt;
+  uint16_t reading;
   uint16_t r0 = 10000;
   uint16_t r1 = 10000;
   float t1 = 25+273.15;
   float t2 = 50+273.15;
   float t3 = 100+273.15;
-  double res_50b;
-  double res_100b;
-  double L1;
-  double L2;
-  double L3;
-  double Y1;
-  double Y2;
-  double Y3;
-  double X2;
-  double X3;
   double A;
   double B;
   double C;
-  res_50b = r0*exp(((1/(t2))-(1/298.15))*B25_50);
-  res_100b = r0*exp(((1/(t3))-(1/298.15))*B25_100);
+ 
 
-  L1 = log(r0);
-  L2 = log(res_50b);
-  L3 = log(res_100b);
+  C = (((((1/t3)-(1/t1))/(log((r0*exp(((1/(t3))-(1/298.15))*B25_100)))-log(r0)))-(((1/t2)-(1/t1))/(log((r0*exp(((1/(t2))-(1/298.15))*B25_50)))-log(r0))))/(log((r0*exp(((1/(t3))-(1/298.15))*B25_100)))-log((r0*exp(((1/(t2))-(1/298.15))*B25_50)))))/(log(r0)+log((r0*exp(((1/(t2))-(1/298.15))*B25_50)))+log((r0*exp(((1/(t3))-(1/298.15))*B25_100))));
+  B = (((1/t2)-(1/t1))/(log((r0*exp(((1/(t2))-(1/298.15))*B25_50)))-log(r0)))-C*(log(r0)*log(r0)+log(r0)*log((r0*exp(((1/(t2))-(1/298.15))*B25_50)))+log((r0*exp(((1/(t2))-(1/298.15))*B25_50)))*log((r0*exp(((1/(t2))-(1/298.15))*B25_50))));
+  A = (1/t1)-log(r0)*(B+C*log(r0)*log(r0));
 
-  Y1 = 1/t1;
-  Y2 = 1/t2;
-  Y3 = 1/t3;
+
   
-  X2=(Y2-Y1)/(L2-L1);
-  X3=(Y3-Y1)/(L3-L1);
-
-  C = ((X3-X2)/(L3-L2))/(L1+L2+L3);
-  B = X2-C*(L1*L1+L1*L2+L2*L2);
-  A = Y1-L1*(B+C*L1*L1);
-
 for(int i = 0; i < 5; i++){
-  if(analogRead(pin) != 0 && analogRead(pin) != 4095){
-  rt = r1 * (4095.00/analogRead(pin)-1.00);
+  reading = analogRead(pin);
+  if(reading == 0){
+    reading = 1;
+  } else if(reading == 4095){
+    reading = 4094;
+  }
+  
+  rt = r1 * (4095.00/double(reading)-1.00);
   temp += 1/(A+B*log(rt)+C*(log(rt)*log(rt)*log(rt)))-273.15;
 
-    
-  } else if(analogRead(pin) == 4095){
-  rt = r1 * (4095.00/4094-1.00);
-  temp += 1/(A+B*log(rt)+C*(log(rt)*log(rt)*log(rt)))-273.15;
-  }else { //divide by 0 case
-    
-  rt = r1 * (4095.00/1-1.00);
-  temp += 1/(A+B*log(rt)+C*(log(rt)*log(rt)*log(rt)))-273.15;
-  }
   }
   return temp/5.00;
 }
