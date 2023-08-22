0e58384cd8cc4d771eba6cd6a09981291ba4397b
erics80 <54673850+erics80@users.noreply.github.com>
2023-08-22 15:34:37 -0400

    Update thermistor.h

--- thermistor.h
+++ thermistor.h
@@ -1,6 +1,13 @@
 #ifndef THERMISTOR_H
 #define THERMISTOR_H
 
+struct thermistor{
+int pin;
+int beta;
+int B25_50;
+int B25_100;
+};
+
 double simpleThermistorRead(int pin, double beta);
 double thermistorRead(int pin, int B25_50, int B25_100);
 
