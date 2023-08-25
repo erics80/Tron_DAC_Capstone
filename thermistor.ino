aa1c3064db9bcc4485a3377a4a02f3cf2989e51b
erics80 <54673850+erics80@users.noreply.github.com>
2023-08-25 08:17:20 -0400

    Update thermistor.ino

--- thermistor.ino
+++ thermistor.ino
@@ -1,18 +1,18 @@
-void setThermistor(thermistor &th, int pin, double beta, int B50, int B100){
-  th.pin = pin;
-  th.beta = beta
-  th.B50 = B50;
-  th.B100 = B100;
+void thermistor::setThermistor(int pin, double beta, int B50, int B100){
+  t_pin = pin;
+  t_beta = beta;
+  t_B50 = B50;
+  t_B100 = B100;
 }
 
-double simpleThermistorRead(thermistor th){
+double thermistor::simpleThermistorRead(){
   double temp;
   double rt;
   double reading;
   uint16_t r0 = 10000;
   uint16_t r1 = 10000;
   for(int i = 0; i < 5; i++){
- reading = analogRead(th.pin);
+ reading = analogRead(t_pin);
   if(reading == 0){
     reading = 1;
   } else if(reading == 4095){
@@ -20,14 +20,14 @@ double simpleThermistorRead(thermistor th){
   }
     
   rt = r1 * (4095.00/reading-1.00);
-  temp += (1/((1/298.15)+(1/th.beta) * log(rt/r0)))-273.15;
+  temp += (1/((1/298.15)+(1/t_beta) * log(rt/r0)))-273.15;
 
 
   }
   return temp/5.00;
 }
 
-double thermistorRead(thermistor th){
+double thermistor::thermistorRead(){
   double temp;
   double rt;
   uint16_t reading;
@@ -41,14 +41,14 @@ double thermistorRead(thermistor th){
   double C;
  
 
-  C = (((((1/t3)-(1/t1))/(log((r0*exp(((1/(t3))-(1/298.15))*th.B100)))-log(r0)))-(((1/t2)-(1/t1))/(log((r0*exp(((1/(t2))-(1/298.15))*th.B50)))-log(r0))))/(log((r0*exp(((1/(t3))-(1/298.15))*th.B100)))-log((r0*exp(((1/(t2))-(1/298.15))*th.B50)))))/(log(r0)+log((r0*exp(((1/(t2))-(1/298.15))*th.B50)))+log((r0*exp(((1/(t3))-(1/298.15))*th.B100))));
-  B = (((1/t2)-(1/t1))/(log((r0*exp(((1/(t2))-(1/298.15))*th.B50)))-log(r0)))-C*(log(r0)*log(r0)+log(r0)*log((r0*exp(((1/(t2))-(1/298.15))*th.B50)))+log((r0*exp(((1/(t2))-(1/298.15))*th.B50)))*log((r0*exp(((1/(t2))-(1/298.15))*th.B50))));
+  C = (((((1/t3)-(1/t1))/(log((r0*exp(((1/(t3))-(1/298.15))*t_B100)))-log(r0)))-(((1/t2)-(1/t1))/(log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))-log(r0))))/(log((r0*exp(((1/(t3))-(1/298.15))*t_B100)))-log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))))/(log(r0)+log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))+log((r0*exp(((1/(t3))-(1/298.15))*t_B100))));
+  B = (((1/t2)-(1/t1))/(log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))-log(r0)))-C*(log(r0)*log(r0)+log(r0)*log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))+log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))*log((r0*exp(((1/(t2))-(1/298.15))*t_B50))));
   A = (1/t1)-log(r0)*(B+C*log(r0)*log(r0));
 
 
   
 for(int i = 0; i < 5; i++){
-  reading = analogRead(th.pin);
+  reading = analogRead(t_pin);
   if(reading == 0){
     reading = 1;
   } else if(reading == 4095){
