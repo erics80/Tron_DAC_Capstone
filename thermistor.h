5ba026b0d158df6f7e98014534ea2d56cf007947
erics80 <54673850+erics80@users.noreply.github.com>
2023-08-25 10:03:48 -0400

    Update thermistor.h

--- thermistor.h
+++ thermistor.h
@@ -2,14 +2,13 @@
 #define THERMISTOR_H
 
 class thermistor{
+
 private:
 int t_pin;
 double t_beta;
-int t_B50;
-int t_B100;
+
 public:
-void setThermistor(int pin, double beta, int B50, int B100);
-double simpleThermistorRead();
+void setThermistor(int pin, double beta);
 double thermistorRead();
 };
 
