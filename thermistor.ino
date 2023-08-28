7ad86e9f18078b8bcff4c81c56373d063348c536
erics80 <54673850+erics80@users.noreply.github.com>
2023-08-28 16:27:24 -0400

    Got rid of A,B,C thermistorRead

--- thermistor.ino
+++ thermistor.ino
@@ -1,65 +1,35 @@
 void Thermistor::setThermistor(int pin, double beta){
-  t_pin = pin;
-  t_beta = beta;
+  _pin = pin;
+  _beta = beta;
 }
 
-double Thermistor::thermistorRead(){
-  double temp;
-  double rt;
-  double reading;
-  uint16_t r0 = 10000;
-  uint16_t r1 = 10000;
-  for(int i = 0; i < 5; i++){
- reading = analogRead(t_pin);
+float Thermistor::thermistorRead(){
+  const int8_t baseTemp = 25;
+  const int8_t numReadings = 5; //The number of readings to average
+  const int16_t r0 = 10000; //Thermistor resistance value at 25 degrees Celsius
+  const int16_t r1 = 10000; //Fixed Resistor value in the voltage divider circuit
+  float temp;
+  float sum;
+  float rt; //Thermistor resistance
+  int16_t reading; //Current ADC Reading value
+  
+
+  //Loop to Read the pin a certain amount of times and average the value
+  for(int i = 0; i < numReadings; i++){
+ reading = analogRead(_pin);
+    
+  //A reading of zero will cause a divide by 0 error
   if(reading == 0){
     reading = 1;
-  } else if(reading == 4095){
-    reading = 4094;
   }
     
+  //Voltage divider equation to solve for thermistor resistance
   rt = r1 * (4095.00/reading-1.00);
-  temp += (1/((1/298.15)+(1/t_beta) * log(rt/r0)))-273.15;
-
-
+    
+  //Equation to convert thermistor resistance to temperature
+  temp = (1/((1/(273.15+baseTemp))+(1/_beta) * log(rt/r0)))-273.15;
+  sum += temp;
   }
-  return temp/5.00;
+  return sum/numReadings;
 }
-
-
-/*
-double Thermistor::thermistorRead(){
-  double temp;
-  double rt;
-  uint16_t reading;
-  uint16_t r0 = 10000;
-  uint16_t r1 = 10000;
-  float t1 = 298.15f;
-  float t2 = 323.15f;
-  float t3 = 373.15f;
-  double A;
-  double B;
-  double C;
- 
-
-  C = (((((1/t3)-(1/t1))/(log((r0*exp(((1/(t3))-(1/298.15))*t_B100)))-log(r0)))-(((1/t2)-(1/t1))/(log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))-log(r0))))/(log((r0*exp(((1/(t3))-(1/298.15))*t_B100)))-log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))))/(log(r0)+log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))+log((r0*exp(((1/(t3))-(1/298.15))*t_B100))));
-  B = (((1/t2)-(1/t1))/(log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))-log(r0)))-C*(log(r0)*log(r0)+log(r0)*log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))+log((r0*exp(((1/(t2))-(1/298.15))*t_B50)))*log((r0*exp(((1/(t2))-(1/298.15))*t_B50))));
-  A = (1/t1)-log(r0)*(B+C*log(r0)*log(r0));
-
-
-  
-for(int i = 0; i < 5; i++){
-  reading = analogRead(t_pin);
-  if(reading == 0){
-    reading = 1;
-  } else if(reading == 4095){
-    reading = 4094;
-  }
-  
-  rt = r1 * (4095.00/double(reading)-1.00);
-  temp += 1/(A+B*log(rt)+C*(log(rt)*log(rt)*log(rt)))-273.15;
-
-  }
-  return temp/5.00;
-}
-*/
 
